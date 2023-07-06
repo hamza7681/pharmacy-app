7ebb634fe1fd4f329f0d98a1eb92c63724d0585b
hamza <hamzambf@gmail.com>
2023-07-06 15:43:44 +0500

    Initial Setup

---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Hello Medical</h1>;
+};
+
+export default Home;
---
+++ app/product/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleProduct = ({ params }) => {
+  return <h1>SingleProduct {params.id}</h1>;
+};
+
+export default SingleProduct;
